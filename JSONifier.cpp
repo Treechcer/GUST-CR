070b58f52f6a1a31e392a85508b5576a923c0b36
Treecher <treechcer@gmail.com>
2025-10-24 21:03:36 +0200

    started working on JSON parser, it's the only working thing for now lol, not fully yet tho, ver 0.0.1

---
+++ JSONifier.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <fstream>
+#include <string> 
+#include <sstream>
+#include <vector>
+#include <stdexcept>
+#ifdef _WIN32
+    #include <windows.h>
+#endif
+
+std::string ver = "0.0.1";
+
+class JSON{
+    public:
+        std::string name;
+        std::string value;
+
+    JSON(std::string name, std::string value){
+        this->name = name;
+        this->value = value;
+    }
+};
+
+std::vector <std::vector<std::string>> parse(std::string contentOfJSON);
+
+int main(){
+    #ifdef _WIN32
+        SetConsoleOutputCP(CP_UTF8);
+    #endif
+
+    std::ifstream file("test.json");
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    std::string content = buffer.str();
+    file.close();
+
+    std::cout << content << std::endl;
+
+    //JSON test("test", "value");
+    //std::cout << test.name << std::endl;
+    //std::cout << test.valueBP << std::endl;
+
+    std::vector <std::vector <std::string>> parsed = parse(content);
+
+    return 0;
+}
+
+std::vector <std::vector<std::string>> parse(std::string contentOfJSON){
+    std::vector <std::string> names;
+    std::vector <std::string> values;
+    bool wasColon = false;
+
+    while (contentOfJSON.length() > 0){
+        char c = contentOfJSON[0];
+        if (c == ' ' || c == '{' || c == '}' || c == '\n') {
+            contentOfJSON.erase(0,1);
+            continue;
+        }
+
+        if (c == ':') {
+            contentOfJSON.erase(0,1);
+            wasColon = true;
+            continue;
+        }
+        if (c == ',') {
+            contentOfJSON.erase(0,1);
+            wasColon = false;
+            continue;
+        }
+
+        if (c == '"'){
+            std::string varName = "";
+            while (true){
+                contentOfJSON.erase(0,1);
+                char ch = contentOfJSON[0];
+                if (ch == '"') break; 
+
+                varName.push_back(ch);
+            }
+            if (wasColon) values.push_back(varName);
+            else names.push_back(varName);
+        }
+        else if((c >= '0' && c <= '9') || c == '-'){
+            std::string value = "";
+            while (true){
+                char ch = contentOfJSON[0];
+                if (ch == ',' || ch == '\n') break; 
+
+                value.push_back(ch);
+                contentOfJSON.erase(0,1);
+            }
+            values.push_back(value);
+            continue;
+        }
+        else{
+            if (contentOfJSON[0] == 't' && contentOfJSON[1] == 'r' && contentOfJSON[2] == 'u' && contentOfJSON[3] == 'e'){
+                if (!wasColon) throw std::invalid_argument("not valid JSON");
+                values.push_back("true");
+                contentOfJSON.erase(0,4);
+            }
+            else if (contentOfJSON[0] == 'f' && contentOfJSON[1] == 'a' && contentOfJSON[2] == 'l' && contentOfJSON[3] == 's' && contentOfJSON[4] == 'e'){
+                if (!wasColon) throw std::invalid_argument("not valid JSON");
+                values.push_back("false");
+                contentOfJSON.erase(0,5);
+            }
+            else{
+                throw std::invalid_argument("not valid JSON");
+            }
+            continue;
+        }
+
+        contentOfJSON.erase(0,1);
+    }
+
+    //for (int i = 0; i < names.size(); i++){
+    //    std::cout << names[i] << std::endl;
+    //}
+
+    //for (int i = 0; i < values.size(); i++){
+    //    std::cout << values[i] << std::endl;
+    //}
+
+    std::vector <std::vector <std::string>> returningVec;
+    returningVec.push_back(names);
+    returningVec.push_back(values);
+
+    return returningVec;
+}
